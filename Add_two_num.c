56e892896cf85e653d15e3e42a64f413099e7847
Neeraj Yadav <neerajydv1113@gmail.com>
2025-01-02 16:26:30 +0530

    Add files via upload

---
+++ Add_two_num.c
@@ -0,0 +1,12 @@
+
+   #include<stdio.h>
+   
+   int main()
+   
+   {
+      int num1,num2;
+      printf("Enter two numbers\n");
+      scanf("%d %d",&num1,&num2);
+      printf("Sum of %d and %d  =  %d ",num1,num2,num1+num2);
+      return 0;
+   }
