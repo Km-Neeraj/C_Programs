3a4e2d037002f19c4e14c5e949f66ea254de1ee3
Neeraj Yadav <neerajydv1113@gmail.com>
2025-01-04 04:13:28 +0530

    Add files via upload

---
+++ swap_two_num.c
@@ -0,0 +1,18 @@
+
+
+     #include<stdio.h>
+      int main()
+      {
+                         int num1,num2;
+                         printf("Enter two numbers\n");
+                         scanf("%d %d",&num1,&num2);
+                         printf("Numbers Before swapping \n");
+                         printf("num1 = %d  num2 = %d ",num1,num2);
+                         num1=num1+num2;
+                         num2=num1-num2;
+                         num1=num1-num2;
+                         printf(" \nNumbers after swapping\n");
+                         printf("num1 = %d  num2 = %d ",num1,num2);
+                         return 0;
+
+      }
