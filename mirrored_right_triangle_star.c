dd1773f7d821a16c97923e287da6bb2ff097bbcb
Neeraj Yadav <neerajydv1113@gmail.com>
2025-01-03 09:04:53 +0530

    Add files via upload

---
+++ mirrored_right_triangle_star.c
@@ -0,0 +1,33 @@
+
+           #include<stdio.h>
+           int main()
+           {
+                         int row,i,j,k;
+                         printf("Enter the number of row\n");
+                         scanf("%d",&row);
+                         for(i=1;i<=row;i++)
+                         {
+                                 for(j=row;j>=i;j--)
+                                 {
+                                                  printf(" ");  // for space
+                                 }
+                                 for(k=1;k<=i;k++)
+                                 {
+                                                  printf("*");    // print star
+                                 }
+                                 printf("\n");
+                         
+                         }
+                 
+                 return 0;
+          
+           }                                              /*      *
+                                                                * *
+                                                              * * *
+                                                            * * * *
+                                                          * * * * *
+                                                        * * * * * *    */  
+                                                                        
+                                                                       
+                                                                
+                                                                      
