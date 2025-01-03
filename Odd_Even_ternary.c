dc47cad8320c601b1bd2b51b08290b63cf995577
Neeraj Yadav <neerajydv1113@gmail.com>
2025-01-04 04:23:43 +0530

    Add files via upload

---
+++ Odd_Even_ternary.c
@@ -0,0 +1,11 @@
+
+     #include<stdio.h>
+     int main()
+     {
+                         int num;
+                         printf("Enter the number\n");
+                         scanf("%d",&num);
+                         num%2==0?printf("%d is even\n",num):printf("%d is odd",num);
+                         
+                         
+   }
