5173547ee3abe76f7b5586802ca64ffff208a571
Neeraj Yadav <neerajydv1113@gmail.com>
2025-01-06 19:06:21 +0530

    Add files via upload

---
+++ getch_opration.c
@@ -0,0 +1,16 @@
+
+   #include<stdio.h>
+   #include<conio.h>
+   int main()
+   {
+                         printf("Hii before 1 getch()");
+                             getch();
+                         printf("\nHello  after 1 getch()");
+                         getch();
+                         printf("\nWelcome to the c after 2 getch()");
+                         getch();
+                         printf("\nFirst program in c");
+                         getch();
+                         
+       return 0;
+   }
