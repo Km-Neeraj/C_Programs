bbf4ca041a4dbd2cb835cf8e4daceda41fbd7007
Neeraj Yadav <neerajydv1113@gmail.com>
2025-01-06 20:26:05 +0530

    Add files via upload

---
+++ Diamond_pattern.c
@@ -0,0 +1,35 @@
+
+     #include<stdio.h>
+     int main()
+     {
+        int i,j,k,row;
+        printf("Enter the number of row\n");
+        scanf("%d",&row);
+        for(i=1;i<=row;i++)
+        {
+                         for(j=row;j>i;j--)
+                         {
+                            printf(" ");
+                         }
+                         for(k=1;k<=i;k++)
+                         {
+                             printf("* ");
+                         }
+                         printf("\n");
+        }
+        for(i=row;i>=1;i--)
+        {
+                         for(j=row;j>=i;j--)
+                         {
+                            printf(" ");
+                         }
+                         for(k=2;k<=i;k++)
+                         {
+                             printf("* ");
+                         }
+                         printf("\n");
+        }
+                 
+        return 0;
+        
+     }
