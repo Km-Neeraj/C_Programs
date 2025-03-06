51af9f49f144dc6eb85e34aad227a95d3bc17410
Neeraj Yadav <neerajydv1113@gmail.com>
2025-03-06 23:33:14 +0530

    Add files via upload

---
+++ nullpointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int *ptr = NULL;  // Creating a null pointer
+
+    if (ptr == NULL) {
+        printf("The pointer is NULL.\n");
+    } else {
+        printf("The pointer is not NULL.\n");
+    }
+
+    return 0;
+}
